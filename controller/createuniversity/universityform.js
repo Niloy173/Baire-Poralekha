4d1827281866cd68bb4c130b881f1c5fb172c975
Niloy173 <niloysaha173@gmail.com>
2022-09-27 03:33:36 +0600

    functionality for university creation

--- controller/createuniversity/universityform.js
+++ controller/createuniversity/universityform.js
@@ -6,9 +6,6 @@ const { UniversityModel } = require("../../model/UniversitySchema");
 const { default: mongoose } = require("mongoose");
 
 function StoreUniversityFormData(req, res, next) {
-  const grad_program = req.body.graduate.split(/\r?\n/);
-  const undergrad_program = req.body.undergraduate.split(/\r?\n/);
-
   const UpdatedroutePath = req.originalUrl.split("/").reverse()[0];
 
   if (UpdatedroutePath === "universitydata-update") {
@@ -23,8 +20,6 @@ function StoreUniversityFormData(req, res, next) {
           type: req.body.type.trim(),
           location: req.body.location.trim(),
           introduction: req.body.universityintro.trim(),
-          UnderGradProgram: undergrad_program,
-          GradProgram: grad_program,
         },
       },
 
@@ -34,7 +29,7 @@ function StoreUniversityFormData(req, res, next) {
         if (!err) {
           setTimeout(() => {
             res.redirect(
-              `/admin/all-university-dashboard/${currentUniversityId}/prospectus-update`
+              `/admin/all-university-dashboard/${currentUniversityId}/undergraduation-prospectus-update`
             );
           }, 1000);
         }
@@ -50,8 +45,6 @@ function StoreUniversityFormData(req, res, next) {
           type: req.body.type.trim(),
           location: req.body.location.trim(),
           introduction: req.body.universityintro.trim(),
-          UnderGradProgram: undergrad_program,
-          GradProgram: grad_program,
         },
       },
 
@@ -60,7 +53,7 @@ function StoreUniversityFormData(req, res, next) {
       function (err, data) {
         if (!err) {
           setTimeout(() => {
-            res.redirect("/admin/prospectusupdate");
+            res.redirect("/admin/undergraduate-prospectus");
           }, 1000);
         }
       }
