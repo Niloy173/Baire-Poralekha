ad8ce61b3df1011982b93c92390bdfd77900588b
Coder-Towhid <islamsmtowhidul@gmail.com>
2022-09-27 13:11:34 +0600

    towhid

--- routes/adminRoute/admin.route.js
+++ routes/adminRoute/admin.route.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 /* internal package */
-const {ArticleModel}= require("../../model/CreateArticleSchema")
+
 const {
   decorateHtmlResponse,
 } = require("../../middlewares/common/decorateHtmlResponse");
