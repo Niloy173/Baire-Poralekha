05a364c3e700a31a3f1057cab8793584870405c7
Niloy173 <niloysaha173@gmail.com>
2022-10-04 03:28:28 +0600

    article-list & homepage article view modified

--- routes/userRoute/article-list.route.js
+++ routes/userRoute/article-list.route.js
@@ -4,11 +4,22 @@ const {
   decorateHtmlResponse,
 } = require("../../middlewares/common/decorateHtmlResponse");
 
+const { ArticleModel } = require("../../model/CreateArticleSchema");
+
 const router = express.Router();
 
-router.get("/article-list", decorateHtmlResponse("Articles"), (req, res) => {
-  res.render("article-list");
-});
+router.get(
+  "/article-list",
+  decorateHtmlResponse("Articles"),
+  async (req, res) => {
+    const data = await ArticleModel.find({ category: "Article" }).sort(
+      "-createdAt"
+    );
+    res.render("article-list", {
+      data,
+    });
+  }
+);
 
 module.exports = {
   router,
